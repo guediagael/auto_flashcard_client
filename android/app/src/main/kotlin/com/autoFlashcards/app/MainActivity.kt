8994a1041f8f255e8e48ca75bc23354c3b60bf95
guediagael <theletchs@gmail.com>
2024-03-14 21:09:23 +0000

    Updated build configs

---
+++ android/app/src/main/kotlin/com/autoFlashcards/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.autoFlashcards.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/auto_flashcards/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.auto_flashcards.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
